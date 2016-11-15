029405c70bdb57e11e9a74c0e39839a622a28406
kuntat <kuntat@users.noreply.github.com>
2016-11-15 23:46:45 +0800

    added table

--- MediaInfoCommenterGUI.py
+++ MediaInfoCommenterGUI.py
@@ -8,6 +8,14 @@ class MediaInfoCommenterGUI(QWidget):
       super(MediaInfoCommenterGUI, self).__init__()
 
       win = QDialog()
+
+      tableWidget = self.makeTable()
+      
+      self.fileBtn = QPushButton("Add files")
+      self.fileBtn.clicked.connect(self.getfiles)
+
+      self.removeBtn = QPushButton("Remove files")
+      self.removeBtn.clicked.connect(self.removefiles)
       
       self.leftlist = QListWidget ()
       self.leftlist.insertItem (0, 'Video Metadata' )
@@ -23,12 +31,16 @@ class MediaInfoCommenterGUI(QWidget):
       self.Stack.addWidget (self.videoMetadataStack)
       self.Stack.addWidget (self.audioMetadataStack)
       
-      self.btn1 = QPushButton("Go",win)
+      self.btn1 = QPushButton("Comment Metadata",win)
       self.connect(self.btn1, SIGNAL("clicked()"), self.doit)
 
-      self.btn2 = QPushButton("Clear",win)
+      self.btn2 = QPushButton("Clear Metadata",win)
       self.connect(self.btn2, SIGNAL("clicked()"), self.doit)
-      		
+
+      
+
+      ## layouts
+      
       hbox = QHBoxLayout()
       # by changing the seq where widget is added, can change arrangement of left and right
       hbox.addWidget(self.leftlist)
@@ -36,12 +48,15 @@ class MediaInfoCommenterGUI(QWidget):
       hbox.addStretch()
 
       hboxBtn = QHBoxLayout()
+      hboxBtn.addWidget(self.fileBtn)
+      hboxBtn.addWidget(self.removeBtn)
       hboxBtn.addWidget(self.btn1)
-##      hboxBtn.addStretch()
       hboxBtn.addWidget(self.btn2)
       hboxBtn.setAlignment(Qt.AlignHCenter)
+      
 
       vboxOverall = QVBoxLayout()
+      vboxOverall.addWidget(self.tableWidget)
       vboxOverall.addLayout(hbox)
       vboxOverall.addStretch()
       vboxOverall.addLayout(hboxBtn)
@@ -50,12 +65,21 @@ class MediaInfoCommenterGUI(QWidget):
       self.leftlist.currentRowChanged.connect(self.display)
       self.setGeometry(10,10,550,50)
       self.setWindowTitle('Media Info Commenter')
-      self.connect(self, SIGNAL("lastWindowClosed()"), self.byebye)
       self.show()
 
-   def byebye(self):
-      self.destroyed(0)
-##      self.exit(0)
+   def getfiles(self):
+      dlg = QFileDialog()
+      dlg.setFileMode(QFileDialog.ExistingFiles)
+      filenames = QStringList()
+
+      if dlg.exec_():
+         filenames = dlg.selectedFiles()
+         for filename in filenames:
+            self.tableWidget.setItem(filenames.indexOf(filename), 0, QTableWidgetItem(filenames[filenames.indexOf(filename)]))
+	 
+   def removefiles(self):
+      print 'ok'
+
 		
    def videoMetadataStackUI(self):
       layout = QFormLayout()
@@ -84,6 +108,30 @@ class MediaInfoCommenterGUI(QWidget):
 
    def display(self,i):
       self.Stack.setCurrentIndex(i)
+
+   def makeTable(self):
+      self.tableWidget = QTableWidget()
+      
+      self.tableWidget.setRowCount(10)
+      self.tableWidget.setColumnCount(1)
+      tableHeader = ["Files"]
+      self.tableWidget.setHorizontalHeaderLabels(tableHeader)
+      self.tableWidget.verticalHeader().setVisible(False)
+
+      self.tableWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)
+      self.tableWidget.setSelectionBehavior(QAbstractItemView.SelectRows)
+      self.tableWidget.setSelectionMode(QAbstractItemView.SingleSelection)
+      self.tableWidget.setShowGrid(True)
+      
+      self.tableWidget.horizontalHeader().setStretchLastSection(True)
+      self.tableWidget.verticalHeader().setResizeMode(QHeaderView.Fixed)
+      self.tableWidget.verticalHeader().setDefaultSectionSize(15)
+      self.tableWidget.setAlternatingRowColors(True)
+      self.tableWidget.setStyleSheet("alternate-background-color: white;background-color: grey;")
+      self.tableWidget.setStyleSheet("QTableView {selection-background-color: blue;}")
+
+      return self.tableWidget
+
 		
 class MyPopup(QWidget):
     def __init__(self):
